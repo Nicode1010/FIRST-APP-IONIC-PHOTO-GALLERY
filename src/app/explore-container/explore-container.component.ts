42d493cb3859552852c29108caa1e9fad990b016
Nicode1010 <nicolasrios@unicauca.edu.co>
2024-06-14 11:01:37 -0500

    Photo Gallery Funtional

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
-
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
 @Component({
   selector: 'app-explore-container',
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss'],
 })
+
 export class ExploreContainerComponent {
 
   @Input() name?: string;
